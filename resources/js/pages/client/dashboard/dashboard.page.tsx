fd6cfc483bb72c7fbf650c8df9328ab2b426cab5
Alejandro Vilchez <joalvm@gmail.com>
2025-07-14 18:19:39 -0500

    feat: agrega clase 'shadow-none' a varios componentes de UI para mejorar el estilo visual

--- resources/js/pages/client/dashboard/dashboard.page.tsx
+++ resources/js/pages/client/dashboard/dashboard.page.tsx
@@ -1,3 +1,4 @@
+import Content from '@/layouts/app/components/content.component';
 import Heading from '@/layouts/app/components/heading.component';
 import ClientLayout from '../client.layout';
 
@@ -6,9 +7,9 @@ const breadcrumbs = [{ title: 'Panel de control', href: route('admin.dashboard')
 export default function DashboardPage() {
     return (
         <ClientLayout breadcrumbs={breadcrumbs} title="Panel de control">
-            <div className="h-fullflex-1 flex w-full flex-col overflow-x-auto rounded-xl bg-background">
+            <Content size="full">
                 <Heading title="Panel de control" description="Resumen general de operaciones" />
-            </div>
+            </Content>
         </ClientLayout>
     );
 }
